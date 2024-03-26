df2e907b2cc9b6de0df3422b72206f435dc28b9f
Oseodion <102174239+Oseodion@users.noreply.github.com>
2024-03-26 13:27:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './HomePage'
 // import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './components';
-import Feed from './components/Feed';
+// import Feed from './components/Feed';
 import VideoDetails from './components/VideoDetails';
 import ChannelDetails from './components/ChannelDetails';
-import SearchFeed from './components/SearchFeed
+// import SearchFeed from './components/SearchFeed
 import { blue } from '@mui/material/colors';
 import LoadingScreen from './components/LoadingScreen';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
     <Box backgroundColor={''}>
       <HomePage/>
       <Routes>
-        <Route path='/' exact element={<Feed/>}/>
+{/*         <Route path='/' exact element={<Feed/>}/> */}
         <Route path='/video/:id' element={<VideoDetails/>}/>
         <Route path='/channel/:id' element={<ChannelDetails/>}/>
-        <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+{/*         <Route path='/search/:searchTerm' element={<SearchFeed/>}/> */}
       </Routes>
     </Box>
     {/* < LoadingScreen/> */}
